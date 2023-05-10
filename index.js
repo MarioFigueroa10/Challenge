a8cf2ccd2f4dae563217c3275b07b05875650fb2
MarioFigueroa10 <124302879+MarioFigueroa10@users.noreply.github.com>
2023-05-09 21:26:32 -0600

    Dieño de la aplicación con css y responsive design junto con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,25 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      munheco.src = "./img/encriptado.jpg";
+    } else {
+      munheco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      alert("Debes ingresar un texto");
+    }
+}
