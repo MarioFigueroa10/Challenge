7926b2307088002277ccddad35ca1ba3924d6adb
MarioFigueroa10 <124302879+MarioFigueroa10@users.noreply.github.com>
2023-05-09 21:40:13 -0600

    Adición de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -23,3 +23,30 @@ function encriptar() {
       alert("Debes ingresar un texto");
     }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")   
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+      if (texto.length != 0) { 
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        munheco.src = "./img/desencriptado.jpg";
+      } else {
+        munheco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar un texto");
+      }
+
+}
